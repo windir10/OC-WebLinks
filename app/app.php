a998b1b486e54b905372619dc3aafc6b92bcdf0d
Steven DUMONT <windir10@gmail.com>
2017-05-04 17:28:48 +0200

    add error page with custom template

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\Debug\ErrorHandler;
 use Symfony\Component\Debug\ExceptionHandler;
+use Symfony\Component\HttpFoundation\Request;
 
 // Register global error and exception handlers
 ErrorHandler::register();
@@ -28,3 +29,18 @@ $app['dao.link'] = function ($app) {
 	$linkDAO->setUserDAO($app['dao.user']);
     return $linkDAO;
 };
+
+// Register error handler
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    switch ($code) {
+        case 403:
+            $message = 'Access denied.';
+            break;
+        case 404:
+            $message = 'The requested resource could not be found.';
+            break;
+        default:
+            $message = "Something went wrong.";
+    }
+    return $app['twig']->render('error.html.twig', array('message' => $message));
+});
