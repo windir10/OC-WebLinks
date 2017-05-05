2c24208bd30b58838b2091f98cc78cd8e97d810e
Steven DUMONT <windir10@gmail.com>
2017-05-05 21:15:33 +0200

    update db script for users password with silex 2

--- db/users-silex2.sql
+++ db/users-silex2.sql
@@ -1,3 +1,3 @@
-INSERT INTO `t_user` (`user_name`, `user_password`, `user_salt`, `user_role`) VALUES
+INSERT INTO t_user (user_name, user_password, user_salt, user_role) VALUES
 ('JohnDoe2', '$2y$13$F9v8pl5u5WMrCorP9MLyJeyIsOLj.0/xqKd/hqa5440kyeB7FQ8te', 'YcM=A$nsYzkyeDVjEUa7W9K', 'ROLE_USER'),
 ('admin2', '$2y$13$A8MQM2ZNOi99EW.ML7srhOJsCaybSbexAj/0yXrJs4gQ/2BqMMW2K', 'EDDsl&fBCJB|a5XUtAlnQN8', 'ROLE_ADMIN');
